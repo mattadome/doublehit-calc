99fa04b6ecd3ce8c3f1138c19cf292adcd16160a
mattadome <mattadome@gmail.com>
2019-10-31 14:18:14 +0800

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+document.getElementById('txt-twicerow').onkeydown = function(e){
+    e.preventDefault();
+}
+document.getElementById('txt-atleastonce').onkeydown = function(e){
+    e.preventDefault();
+}
+document.getElementById('txt-never').onkeydown = function(e){
+    e.preventDefault();
+}
+
+
